f3f0180d81ff60c750c1d92b4758d5c9abe0bf1c
shusterboris <shuster.boris@gmail.com>
2021-08-09 10:59:05 +0300

    Версия 0.5.0

---
+++ src/components/SchedulePlan.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export class EmptyPage extends Component {
+
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return <div className="p-grid">
+            <div className="p-col-12">
+                <div className="card">
+                    <div className="card-title">Empty Page</div>
+                    <p>Начинаем отсюда</p>
+                </div>
+            </div>
+        </div>
+    }
+}
